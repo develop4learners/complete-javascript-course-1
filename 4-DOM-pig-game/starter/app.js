d0c86256e494a2742f0de926801d54ee64a96b6e
develop4learners <cqr2@cornell.edu>
2020-02-02 16:19:22 -0800

    completed my soln to pig game challenge part 2, players set their own winning score

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,42 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var dice, currentPlayer, scores, roundScore;
+scores = [0, 0];
+dice = Math.floor(Math.random()*6)+1;
+activePlayer = 1;
+roundScore = 0;
+console.log(dice);
+
+document.querySelector('#current-'+activePlayer).textContent = dice;
+
+//Practice adding the roundScore, or just dice, to Player1's global score..
+scores[activePlayer]+=dice;
+document.querySelector('#score-'+activePlayer).textContent = ''+scores[activePlayer];
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  //This is an anonymous function, I could have put the name of this function here, and defined it elsewhere to be reused,
+  //but in our case, we only need it here, so why skip naming it, and just put it here?
+  //1. Random number
+  dice = Math.floor(Math.random()*6)+1;
+  //2. Display the result.
+  document.querySelector('#current-'+activePlayer).textContent = dice;
+  //3. Update the roundScore IF the rolled number was not a 1.
+  if (dice!==1){
+    roundScore += dice;
+    document.querySelector('#current-'+activePlayer).textContent = roundScore;
+
+  } else {
+    scores[activePlayer] = 0;
+    document.querySelector('#score-'+activePlayer).textContent = '0';
+  }
+
+
+
+
+
+
+
+})
